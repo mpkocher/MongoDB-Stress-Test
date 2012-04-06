192daf07b79fb35e1b134ef6f5d6b4c25b2aa2f5
dangunter <dkgunter@lbl.gov>
2012-04-06 09:18:32 -0700

    added sleepy mongoose capabilities

--- w.py
+++ w.py
@@ -11,6 +11,10 @@ import sys
 import time
 
 import pymongo
+try:
+    import mongate
+except ImportError:
+    mongate = None
 
 my_hostname = socket.getfqdn()
 my_pid = os.getpid()
@@ -25,14 +29,12 @@ REPORT_COLL = 'report'
 
 wait_for_it = 0
 net_timeout = 3600 # default to 1 hr
-g_conn = None # global connection
 
-def stress_test(ndocs=1, db_name=None, coll_name=None):
+def stress_test(connections, ndocs=None, db_name=None, coll_name=None):
     """Run a stress test.
     Returns: duration, in seconds
     """
-    collection = g_conn[db_name][coll_name]
-    
+    collections = [x[db_name][coll_name] for x in connections]
     t0 = time.time()
     if wait_for_it > 0:
         wait_sleep = int(max(0,wait_for_it - t0))
@@ -43,18 +45,20 @@ def stress_test(ndocs=1, db_name=None, coll_name=None):
     log.debug("loop.start n={n}".format(n=ndocs))
     t0 = time.time()
     for i in xrange(ndocs):
-        doc = {'doc_num': i, 'created_at': datetime.datetime.now(), 'message': message}
-        collection.insert(doc)
+        doc = {'doc_num': i, 'message': message}
+        for collection in collections:
+            collection.insert(doc)
     dur = time.time() - t0
     log.debug("loop.end n={n} dur={d:f}".format(n=ndocs, d=dur))
     return dur
 
-def report(run, delta_time, **kw):
+def report(conn, run, delta_time, **kw):
     """Report time for a client.
     """
     doc = {"host":my_hostname, "pid":my_pid, "run":run, "dt":delta_time}
     doc.update(kw)
-    coll = g_conn[REPORT_DB][REPORT_COLL]
+    log.debug("reporting {}".format(doc))
+    coll = conn[REPORT_DB][REPORT_COLL]
     coll.insert(doc)
 
 def print_results(coll): 
@@ -69,7 +73,7 @@ def print_results(coll):
 def main():
     """Program entry point.
     """
-    global wait_for_it, g_conn
+    global wait_for_it
 
     # command-line
     parser = OptionParser()
@@ -77,14 +81,18 @@ def main():
                       action="store_true", default=False)
     parser.add_option('-d', '--ndocs', dest='ndocs', metavar="NUM", type='int', default= 1000,
                       help='Insert NUM docs per client (default=%default)')
+    parser.add_option('-m', '--mongoose', dest='mongoose', action='store_true',
+                      help="Use sleepy mongoose REST api instead of mongodb native protocol")
+    parser.add_option('-n', '--nclients', dest='nclients', metavar="NUM", type='int', default=1,
+                      help="Number of clients to connect to server (default=%default)")
+    parser.add_option('-p', '--port', dest='port', type='int', default=27018,
+                      help='Connect to MongoDB server on PORT (default=%default)')
+    parser.add_option('-q', '--quiet', dest='quiet', action='store_true', help="No logging")
     parser.add_option('-r', '--results', dest='do_check', help="Print results and exit. "
                       "With -c/--clear also clears results.",
                       action="store_true")
     parser.add_option("-R", "--run", dest="runid", metavar="ID", help="Run identifier", default=None)
     parser.add_option('-s', '--server', dest='host', help='MongoDB server host (required)')
-    parser.add_option('-p', '--port', dest='port', type='int', default=27018,
-                      help='Connect to MongoDB server on PORT (default=%default)')
-    parser.add_option('-q', '--quiet', dest='quiet', action='store_true', help="No logging")
     parser.add_option('-v', '--verbose', dest='vb', action='store_true', help="More logging")
     parser.add_option('-w', '--when', dest='when', type='int', default=0,
                       help="Start at future time SEC seconds since 1/1/1970 (default=now)",
@@ -115,40 +123,67 @@ def main():
     elif options.vb:
         log.setLevel(logging.DEBUG)
     else:
-        log.setLevel(logging.INFO)
+        if options.do_check:
+            log.setLevel(logging.WARN)
+        else:
+            log.setLevel(logging.INFO)
 
     # get vars from options
     db_name, coll_name = STRESS_DB, STRESS_COLL
-    ndocs, host, port, wait_for_it = (options.ndocs, options.host, options.port, options.when)
+    ndocs, host, port, wait_for_it, ncli = (options.ndocs, options.host,
+                                     options.port, options.when, options.nclients)
 
     # start
-    log.info("run.start docs={m} server={h}:{p:d} "
+    log.info("run.start docs={m} clients={c} server={h}:{p:d} "
              "db={db} collection={coll}".format(
-             r=run_id, m=ndocs, h=host, p=port, db=db_name, coll=coll_name))
+             r=run_id, m=ndocs, h=host, p=port, db=db_name,
+             coll=coll_name, c=ncli))
 
     log.debug("pre.start")
-    # connect
-    g_conn = pymongo.Connection(host, port)
-    # check mode, just print results and stop
+    # connect (once for each client)
     if options.do_check:
-        coll = g_conn[db_name][REPORT_COLL]
+        ncli = 1
+    if options.mongoose:
+        # Assume sleepymongoose is listening on port 27080
+        # and connecting via localhost to the mongodb server.
+        if mongate is None:
+            parser.error("-m/--mongoose option requires 'mongate' Python "
+                    "module, which was not found.")
+            return 2
+        from mongate import connection
+        try:
+            connections = [connection.Connection(host, 27080)
+                       for _ in xrange(ncli)]
+        except socket.error, err:
+            log.critical("mongoose.connection.error msg={}".format(err))
+            return -1
+        for conn in connections:
+            conn.connect_to_mongo("mongodb://localhost", port)
+    else:
+        connections = [pymongo.Connection(host, port)
+                      for _ in xrange(ncli)]
+    # check mode, just print results and stop
+    conn = connections[0]
+    if options.do_check:
+        coll = conn[db_name][REPORT_COLL]
         print_results(coll)
         if options.do_clear:
-            g_conn[db_name][REPORT_COLL].remove()
+            conn[db_name][REPORT_COLL].remove()
         log.debug("pre.end status=0")
         return 0
     # with clear flag, empty db first
     if options.do_clear:
-        g_conn[db_name][coll_name].remove()
+        conn[db_name][coll_name].remove()
     log.debug("pre.end status=0")
 
     log.debug("main.start")
-    dur = stress_test(ndocs=ndocs, db_name=db_name, coll_name=coll_name)
+    dur = stress_test(connections, ndocs=ndocs, db_name=db_name,
+                      coll_name=coll_name)
     log.debug("main.end status=0")
 
     log.debug("post.start")
     # re-use a previous connection
-    report(run_id, dur, ndocs=ndocs)
+    report(conn, run_id, dur, docs=ndocs, clients=ncli)
     log.debug("post.end status=0")
 
     # done
